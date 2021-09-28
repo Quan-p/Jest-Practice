bd643c7cf131b0b355a02a582ab806eeba26b63c
Quan-p <quanp155@gmail.com>
2021-09-28 16:09:43 -0500

    Changed to import

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,17 +1,17 @@
-const calculator = require('../code/calculator');
+import calculator from '../code/calculator';
 
-test('Adds 1 and 2 to equal 3', () => {
-    expect(calculator.add(1, 2).toBe(3));
+test('Adds two numbers', () => {
+    expect(calculator.add(10, 5)).toBe(15);
 });
 
-test('Subtract 1 and 2 to equal -1', () => {
-    expect(calculator.subtract(1, 2).toBe(-1));
+test('Subtracts two numbers', () => {
+    expect(calculator.subtract(10, 5)).toBe(5);
 });
 
-test('Multiply 5 and 2 to equal ', () => {
-    expect(calculator.multiply(5, 2).toBe(10));
+test('Multiply two numbers', () => {
+    expect(calculator.multiply(10, 5)).toBe(50);
 });
 
-test('Divide 100 and 2 to equal 50', () => {
-    expect(calculator.divide(100, 2).toBe(50));
+test('Divides two numbers', () => {
+    expect(calculator.divide(10, 5)).toBe(2);
 });
