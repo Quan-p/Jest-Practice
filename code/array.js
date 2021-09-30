9138b637d71572bb1aedb370162cfadd2a2465d5
Quan-p <quanp155@gmail.com>
2021-09-29 20:42:09 -0500

    Return object with all 4 array values

--- code/array.js
+++ code/array.js
@@ -1,3 +1,10 @@
-const arrayAnalysis = (array);
+const arrayAnalysis = (array) => {
+    return {
+        min: Math.min(...array),
+        max: Math.max(...array),
+        average: array.reduce((a, b) => a + b) / array.length,
+        length: array.length,
+    }
+};
 
 export default arrayAnalysis;
