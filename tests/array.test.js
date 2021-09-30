d2d61bac32f18c989010fe4df9004a49e1161a36
Quan-p <quanp155@gmail.com>
2021-09-29 20:41:26 -0500

    Restructured arrow functions

--- tests/array.test.js
+++ tests/array.test.js
@@ -1,17 +1,9 @@
 import arrayAnalysis from '../code/array';
 
-test('Check for min value', () => {
-    expect(arrayAnalysis([1, 4, 5, 6, 8, 12]).min).toBe(1);
-});
+test('Check for min value', () => expect(arrayAnalysis([1, 4, 5, 6, 8, 12]).min).toBe(1));
 
-test('Check for max value', () => {
-    expect(arrayAnalysis([1, 4, 5, 6, 8, 12]).min).toBe(12);
-});
+test('Check for max value', () => expect(arrayAnalysis([1, 4, 5, 6, 8, 12]).max).toBe(12));
 
-test('Check for average value', () => {
-    expect(arrayAnalysis([1, 4, 5, 6, 8, 12]).min).toBe(6);
-});
+test('Check for average value', () => expect(arrayAnalysis([1, 4, 5, 6, 8, 12]).average).toBe(6));
 
-test('Check for length', () => {
-    expect(arrayAnalysis([1, 4, 5, 6, 8, 12]).min).toBe(6);
-});
+test('Check for length', () => expect(arrayAnalysis([1, 4, 5, 6, 8, 12]).length).toBe(6));
